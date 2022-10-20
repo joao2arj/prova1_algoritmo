c4ddadd6cd266fe2930520b5eab013404c22c320
joao2arj <joao2arj@gmail.com>
2022-10-20 12:47:14 -0300

    add q2 (criar menu e switch case)

--- joaopedrofonseca_uc22000347.c
+++ joaopedrofonseca_uc22000347.c
@@ -3,7 +3,33 @@
 #include <math.h>
 
 int main() {
-	printf(" Disciplina: Algoritmo e programacao\n Professor: Joyce Siqueira\n Aluno: Joao Pedro Araujo S. Fonseca\n Matricula: UC22000347\n Curso: Analise e Desenvolvimento de sistemas\n Repositorio (Git): https://github.com/joao2arj/prova_algoritmo\n Software utilizado: DevC++");
+	
+	char q;
+	
+	//questão 1
+	printf(" Disciplina: Algoritmo e programacao\n Professor: Joyce Siqueira\n Aluno: Joao Pedro Araujo S. Fonseca\n Matricula: UC22000347\n Curso: Analise e Desenvolvimento de sistemas\n Repositorio (Git): https://github.com/joao2arj/prova_algoritmo\n Software utilizado: DevC++\n\n");
+	
+	//questão 2
+	printf("##################################################");
+	printf("\n\n Digite a questao que deve ser executada:\n\n \t|A|\t |B|\t |C|\t\n\n");
+	printf("Questao: ");
+	scanf("%c", &q);
+	//system("cls");
+	
+	switch(q){
+		case 'A':
+			break;
+		
+		case 'B':
+			break;
+		
+		case 'C':
+			break;
+			
+		default:
+			printf("AVISO: OPCAO INVALIDA!");
+			
+	}
 	
 	return 0;
 }
