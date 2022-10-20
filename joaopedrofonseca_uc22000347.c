71e6cb4608e4a74a781beedbda3b752564d053ce
joao2arj <joao2arj@gmail.com>
2022-10-20 13:01:39 -0300

    add q2.A (valor da hipotenusa)

--- joaopedrofonseca_uc22000347.c
+++ joaopedrofonseca_uc22000347.c
@@ -5,6 +5,8 @@
 int main() {
 	
 	char q;
+	int a, b;
+	float val_hipotenusa, sen;
 	
 	//questão 1
 	printf(" Disciplina: Algoritmo e programacao\n Professor: Joyce Siqueira\n Aluno: Joao Pedro Araujo S. Fonseca\n Matricula: UC22000347\n Curso: Analise e Desenvolvimento de sistemas\n Repositorio (Git): https://github.com/joao2arj/prova_algoritmo\n Software utilizado: DevC++\n\n");
@@ -14,20 +16,42 @@ int main() {
 	printf("\n\n Digite a questao que deve ser executada:\n\n \t|A|\t |B|\t |C|\t\n\n");
 	printf("Questao: ");
 	scanf("%c", &q);
-	//system("cls");
+	
+	system("cls");
 	
 	switch(q){
 		case 'A':
+			
+			printf("# Questao A: Valor da hipotenusa. #\n\n");
+			
+			printf("Para calcular o valor da hipotenusa, digite o valor do cateto oposto:\n");
+			scanf("%d", &a);
+			system("cls");
+			
+			printf("Agora, o valor do cateto adjacente:\n");
+			scanf("%d", &b);
+			system("cls");
+			
+			val_hipotenusa = sqrt(pow(a, 2) + pow(b, 2));
+			sen = (a / val_hipotenusa);
+			
+			printf(" REPOSTA:\n\n Hipotenusa: %.3lf\n Seno: %.3lf", val_hipotenusa, sen);
+			
+			
 			break;
 		
 		case 'B':
+			printf("Questao B\n\n");
+			
 			break;
 		
 		case 'C':
+			printf("Questao C\n\n");
+			
 			break;
 			
 		default:
-			printf("AVISO: OPCAO INVALIDA!");
+			printf("AVISO: OPCAO INVALIDA!\n");
 			
 	}
 	
