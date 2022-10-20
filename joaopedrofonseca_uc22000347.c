b8a7a894f7a66efd031295584fed967b316bcda2
joao2arj <joao2arj@gmail.com>
2022-10-20 12:18:36 -0300

    add q1

---
+++ joaopedrofonseca_uc22000347.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	printf(" Disciplina: Algoritmo e programacao\n Professor: Joyce Siqueira\n Aluno: Joao Pedro Araujo S. Fonseca\n Matricula: UC22000347\n Curso: Analise e Desenvolvimento de sistemas\n Repositorio (Git): https://github.com/joao2arj/prova_algoritmo\n Software utilizado: DevC++");
+	
+	return 0;
+}
